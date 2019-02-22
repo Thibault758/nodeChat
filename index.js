0c6a8692a68825c53453fca6520c4447d3d19f08
Thibault758 <roux.thibault@gmail.com>
2019-02-22 15:05:07 +0100

    Supprime le doublon PORT

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
 const server = require('http').createServer(app);
 const io = require('socket.io')(server) // Créer une instance de socket.io sur ce serveur
 
